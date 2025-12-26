e59d69af5b65befb0e153cbdd8215cafd788ef3a
saltmoon39 <saltmoon39@gmail.com>
2025-12-26 18:19:12 +0900

    upload kadai007

---
+++ kadai_007/assosiative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>kadai007</title>
+</head>
+
+<body>
+   <p>
+    <?php
+    $product_data = ['name' => 'onion' , 'price' => 200 , 'weight' => 160];
+    print_r($product_data);
+    ?>
+   </p>
+</body>
+</html>
